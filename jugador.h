bcb760498f49b417c5d870bbc4cde157157f1fc0
skdi <jddhghf@gmail.com>
2016-10-19 21:40:58 -0500

    Update jugador.h

--- jugador.h
+++ jugador.h
@@ -1,12 +1,9 @@
 #ifndef JUGADOR_H
 #define JUGADOR_H
 
-#include <QMainWindow>
+
 #include <QGraphicsRectItem>
-#include <QApplication>
-#include <QGraphicsScene>
-#include <QGraphicsView>
-#include <QKeyEvent>
+
 
 class jugador:public QGraphicsRectItem{
 public :
