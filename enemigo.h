ae938a98a4f665ab06893ff85bf77f4191cfc75f
Skdi <jddhghf@gmail.com>
2016-11-03 11:43:18 -0500

    Update enemigo.h

--- enemigo.h
+++ enemigo.h
@@ -1,18 +1,15 @@
-#ifndef ENEMIGO_H
-#define ENEMIGO_H
-
+#ifndef DISPARO_H
+#define DISPARO_H
 #include <QGraphicsRectItem>
 #include <QObject>
 
 
-class enemigo:public QObject,public QGraphicsRectItem{
+class disparo:public QObject,public QGraphicsRectItem{
     Q_OBJECT//macro de tipo objeto para el uso de esta funcion
 public:
-    enemigo();
+    disparo();
 public slots:
     void movimiento();
 };
 
-
-
-#endif // ENEMIGO_H
+#endif // DISPARO_H
