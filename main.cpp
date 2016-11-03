5959c6ce77ce252136c36d9170770c9efc4105c0
Skdi <jddhghf@gmail.com>
2016-11-03 11:42:24 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include "jugador.h"
 #include <QMainWindow>
-
 #include <QApplication>
 #include <QGraphicsScene>
 #include <QGraphicsView>
+#include <QTimer>
 
 
 
@@ -37,6 +37,12 @@ int main(int argc,char *argv[])
     scene->setSceneRect(0,0,800,600);
     //posicion inicial en el medio ,abajo
     jugado->setPos(vista->width()/2 - (jugado->rect().width())/2,vista->height()-jugado->rect().height()-10);
+
+    //spawn de enemigos
+    QTimer *tiempo=new QTimer();
+    QObject::connect(tiempo,SIGNAL(timeout()),jugado,SLOT(spawn()));
+    tiempo->start(5);//delay de creacion del enemigo
+
 
     return a.exec();//retorna un tipo ejecutable del programa
 }
