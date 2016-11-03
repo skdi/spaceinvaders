0ad950fe6e8e21846955f1577f04a2b421491529
Skdi <jddhghf@gmail.com>
2016-11-03 11:42:02 -0500

    Update jugador.cpp

--- jugador.cpp
+++ jugador.cpp
@@ -1,6 +1,8 @@
 #include "jugador.h"
 #include "disparo.h"
+#include "enemigo.h"
 #include <QKeyEvent>
+#include <QGraphicsScene>
 
 
 //movimiento del jugador
@@ -20,11 +22,24 @@ void jugador::keyPressEvent(QKeyEvent *evento)
     else if (evento->key()==Qt::Key_Down){
         setPos(x(),y()+10);
     }*/
+
+    /*else if (evento->key()==Qt::Key_Escape){
+        scene->close();
+    }*/
     else if (evento->key()==Qt::Key_Space){
         //creando bala
         disparo *bala=new disparo();
+        //le doy la posicion (central) respecto a mi "jugador"
         bala->setPos(x()+(rect().width()/2 -5),y());
         scene()->addItem(bala);
     }
 }
+
+void jugador::spawn(){
+    //creando un enemigo
+    enemigo *enemy=new enemigo();
+    scene()->addItem(enemy);
+
+}
+
 
